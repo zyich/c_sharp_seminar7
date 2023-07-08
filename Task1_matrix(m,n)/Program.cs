d1ebb78d093c4a42e889f8d01ce1f6ebd7e9ac04
zyich <zuilinho@yandex.ru>
2023-07-08 13:44:37 +0200

    task1

---
+++ Task1_matrix(m,n)/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Задача 46: Задайте двумерный массив 
+размером m×n, заполненный случайными целыми числами.
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1
+*/
+
+Console.Clear();
+
+
+int [,] CreateArray(int row, int col, int min , int max)
+{
+    Random rand = new Random();
+    int [,] array = new int [row,col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            array[i, j] = rand.Next(min, max+1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int [,] array)
+{
+    int row = array.GetLength(0);
+    int col = array.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+
+
+}
+
+int [,] array = CreateArray(3, 4, -100, 100);
+PrintArray(array);
